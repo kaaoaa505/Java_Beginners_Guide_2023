2da47de55d41978b225196985da2e1afc07d3208
Khaled Allam <kaaoaa505@gmail.com>
2023-10-28 08:49:35 +0300

    first commit

---
+++ 001_TemurinJdk_1_8_FirstProject/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String my_string = "Hello world!, this is my first 1.8 java project test";
+        System.out.println(my_string);
+
+        int my_number = 33;
+        System.out.println(my_number);
+        System.out.println(my_number * my_number);
+        System.out.println(my_number / my_number);
+        System.out.println(my_number + my_number);
+
+        boolean is_it_true = true;
+        System.out.println(is_it_true);
+
+        System.out.println(my_string + ' ' + my_number + " " + is_it_true);
+    }
+}
